b853db31c7f94735678a480b6191d7460dcdd67d
MaximilianKellner <maximilian_elias.kellner@smail.th-koeln.de>
2024-04-26 15:12:46 +0200

    sidebar fix 2

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -1,4 +1,11 @@
+console.log('main.js loaded');
+
+const mainMenueTrigger = document.querySelector('[data-js-main-menue-trigger]');
+mainMenueTrigger.addEventListener('click', handleMenueClick);
+
 function handleMenueClick() {
-    const mainMenue = document.querySelector('[data-js-main-menue-trigger]');
+    console.log('handleMenueClick');
+    const mainMenue = document.querySelector('.main-menu');
     mainMenue.classList.toggle('is-active');
+    mainMenueTrigger.classList.toggle('is-active');
 }
